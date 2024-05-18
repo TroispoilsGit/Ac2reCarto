ff7d05ab2c8ef0dfe9bf950df0e9b8b022708979
troispoils <troispoils@live.com>
2024-05-18 23:45:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,10 +5,11 @@ const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        autoHideMenuBar: true,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
-            enableRemoteModule: true
+            enableRemoteModule: true,
         }
     })
 
